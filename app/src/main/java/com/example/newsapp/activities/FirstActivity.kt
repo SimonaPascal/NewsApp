669d35a8d7b14ee4b64542ff2c5b9d38de74bbce
Simona Pascal <simonapascal98@gmail.com>
2021-05-22 15:49:42 +0300

    RegisterPage + LoginPage + FirstPage

---
+++ app/src/main/java/com/example/newsapp/activities/FirstActivity.kt
@@ -0,0 +1,35 @@
+package com.example.newsapp.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.newsapp.R
+import com.example.newsapp.ui.login.LoginActivity
+
+class FirstActivity : AppCompatActivity() {
+    private lateinit var registerButton: Button
+    private lateinit var loginButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first)
+        init()
+        loginButtonPressed()
+        registerButtonPressed()
+    }
+    fun init(){
+        this.registerButton = findViewById(R.id.register_button_first_page)
+        this.loginButton = findViewById(R.id.login_button_first_page)
+    }
+    fun registerButtonPressed() = this.registerButton.setOnClickListener {
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
+    }
+
+
+    fun loginButtonPressed() = this.loginButton.setOnClickListener {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+}
